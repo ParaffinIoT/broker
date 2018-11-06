26b84a000c7f5c7edb014822acfbfe209fce23b7
ioBoard <hmahdavi@neda.net>
2018-11-06 22:03:28 +0330

    🐼 Update config\parse-server.js

--- config/parse-server.js
+++ config/parse-server.js
@@ -3,8 +3,8 @@
 const logger = require('./components/logger')
 const mongodb = require('./components/mongo')
 const common  = require('./components/common')
-//const server = require('./components/server')
+const server = require('./components/parseserver')
 
 
 //module.exports = Object.assign({}, logger, mongodb, common, server)
-module.exports = Object.assign({}, logger, mongodb, common)
+module.exports = Object.assign({}, logger, mongodb, common, server)
