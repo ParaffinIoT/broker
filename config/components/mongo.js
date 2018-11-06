3ff142993cb015f0a99f6d3168b129a03cd437c5
ioBoard <hmahdavi@neda.net>
2018-11-06 22:00:22 +0330

    🌴 Update mongo.js

--- config/components/mongo.js
+++ config/components/mongo.js
@@ -4,14 +4,18 @@ const joi = require('joi')
 
 const envVarsSchema = joi
   .object({
-    DB_URL: joi
-      .string()
-      .regex(/^mongodb:\/\//)
-      .required(),
+    DB_URL: joi.string().regex(/^mongodb:\/\//).required(),
+    DB_PONTE_NAME: joi.string().required(),
+    DB_AUTH_NAME: joi.string().required(),
+    DB_AUTH_COLLECTION: joi.string().required(),
+    DB_COLLECTION_PREFIX: joi.string().required()
   })
   .unknown()
 
-const { error, value: env } = joi.validate(process.env, envVarsSchema)
+const {
+  error,
+  value: env
+} = joi.validate(process.env, envVarsSchema)
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`)
@@ -19,4 +23,8 @@ if (error) {
 
 module.exports = {
   DB_URL: env.DB_URL,
+  DB_PONTE_NAME: env.DB_PONTE_NAME,
+  DB_AUTH_NAME: env.DB_AUTH_NAME,
+  DB_AUTH_COLLECTION: env.DB_AUTH_COLLECTION,
+  DB_COLLECTION_PREFIX: env.DB_COLLECTION_PREFIX
 }
