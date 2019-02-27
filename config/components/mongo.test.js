500f0409467c93e27950d1e9a302019897c3ee83
ioboard <iokloud.com@gmail.com>
2019-02-27 09:33:24 -0800

    Edit HTTP test

--- config/components/mongo.test.js
+++ config/components/mongo.test.js
@@ -4,15 +4,9 @@ const { expect } = require('chai')
 describe('MongoDb Component Test', () => {
   const componentPath = './mongo'
 
-  it('should throw error if mongodb url is invalid', () => {
-    delete process.env.DB_URL
-    process.env.DB_URL = 'localhost:3000'
-    expect(() => require(componentPath)).to.throws('Config validation error:')
-  })
-
   it('should return mongodb url as env', () => {
     process.env = {
-      DB_URL: 'mongodb://localhost:2707',
+      DB_URL: 'mongodb://localhost:27017',
       DB_PONTE_NAME: 'mongodb://localhost:27017/ponte',
       DB_AUTH_URL: 'mongodb://localhost:27017/',
       DB_AUTH_NAME: 'paraffin',
